abd507889ef12ceb36085e06f618fb46b699e6ad
Mehul Arora <mehularora658@gmail.com>
2023-05-12 17:42:20 +0530

    Hardcoding of calculator UI is completed

--- src/App.js
+++ src/App.js
@@ -3,21 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Calculator App</h1>
+      <form>
+        <input type="text" readOnly className="display" />
+        <br />
+        <div className="btns">
+          <input type="button" className="btn" value="1" />
+          <input type="button" className="btn" value="2" />
+          <input type="button" className="btn" value="3" />
+          <input type="button" className="btn" value="+" />
+          <br />
+          <input type="button" className="btn" value="4" />
+          <input type="button" className="btn" value="5" />
+          <input type="button" className="btn" value="6" />
+          <input type="button" className="btn" value="-" />
+          <br />
+          <input type="button" className="btn" value="7" />
+          <input type="button" className="btn" value="8" />
+          <input type="button" className="btn" value="9" />
+          <input type="button" className="btn" value="*" />
+          <br />
+          <input type="button" className="btn" value="0" />
+          <input type="button" className="ResetBtn" value="Reset" />
+          <input type="button" className="btn" value="/" />
+        </div>
+      </form>
     </div>
   );
 }
